48b838dde56f1f69f79f73f4afca3c16d609212d
Abhishek123-rgb <71871455+Abhishek123-rgb@users.noreply.github.com>
2021-02-24 18:41:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
@@ -9,8 +9,73 @@ const App = () => {
     left: "0px",
     top: "0px",
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+
+  const handler  = (event)=>{
+    let left1 = Number(ballPosition.left.slice(0,-2));
+    let top1 = Number(ballPosition.top.slice(0,-2));
+      switch(event.keyCode){
+        case 39:
+          setBallPosition({
+            left: `${left1 + 5}px`,
+            top : `${top1}px`,
+          });
+          break;
+        case 40:
+          setBallPosition({
+            left: `${left1}px`,
+            top : `${top1+5}px`,
+          });
+          break;
+        case 38:
+          setBallPosition({
+            left: `${left1}px`,
+            top : `${top1-5}px`,
+          });
+          break;
+        case 37:
+          setBallPosition({
+            left: `${left1 - 5}px`,
+            top : `${top1}px`,
+          });
+           break;
+        default:
+           break;
+      }
+      console.log(ballPosition.left,ballPosition.top);
+    };
+
+
+  useEffect(() => {
+    window.addEventListener("keydown", handler);
+    return () => {
+      window.removeEventListener("keydown", handler);
+    }
+  }, [ballPosition]);
+  const buttonClickHandler = () => {
+    setRenderBall(true);
+  };
+  const reset = () => {
+    setBallPosition({
+            left: `0px`,
+            top : `0px`,
+          });
+    setRenderBall(false);
+  };
+  const renderChoice = () => {
+    if(renderBall) {
+      return <div 
+                className = "ball"
+                style = {{
+                  left : ballPosition.left,
+                  top : ballPosition.top,
+                  position :"absolute"
+                }}
+
+            ></div>
+    }else{
+      return <button className = "start" onClick={buttonClickHandler}>Play</button>
+    }
+  };
 
   return (
     <div className="playground">
